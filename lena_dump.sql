8c0d4e516d46fdf087ae972a002e83271ab057f4
Podpivan4ik <ivan.milashka2020@yandex.ru>
2025-11-10 17:21:53 +0300

    Update lena_dump.sql 2

--- lena_dump.sql
+++ lena_dump.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `clients`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clients` (
   `client_id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `first-name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
